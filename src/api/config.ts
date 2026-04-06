5a7bb9d0ebf657c51365eda775d88ef0bd72a638
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2026-04-06 16:25:10 +0100

    Create the config where we add the api client

---
+++ src/api/config.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+// Pull the API URL directly from the .env file.
+// We provide a fallback just in case the .env fails to load.
+const BASE_URL =
+  process.env.EXPO_PUBLIC_API_URL || "http://192.168.100.124:5000/api/v1";
+
+export const apiClient = axios.create({
+  baseURL: BASE_URL,
+  timeout: 40000, // 40 seconds (To account for the AI processing time)
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+// Request Interceptor (Ready for JWT authentication)
+apiClient.interceptors.request.use(
+  async (config) => {
+    // We will activate this when the Auth module is built
+    // const token = await SecureStore.getItemAsync('userToken');
+    // if (token) {
+    //   config.headers.Authorization = `Bearer ${token}`;
+    // }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
