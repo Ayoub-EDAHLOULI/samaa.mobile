a640d5b08e204061fbb6e2a7a120480daa0e0173
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2026-04-06 16:34:32 +0100

    Create the setting page

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function SettingsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Settings</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0F172A",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#FFFFFF",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
